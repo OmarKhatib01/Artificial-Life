44488fc30d73341cd7b6200f136b39f9f32af127
OmarKhatib01 <osokhatib1@gmail.com>
2023-01-29 20:37:27 -0600

    N. quadruped done

--- constants.py
+++ constants.py
@@ -1,10 +1,15 @@
 import numpy
 
-steps = 1000
+steps = 1500
 
-numberOfGenerations = 10
+numberOfGenerations = 15
+populationSize = 15
 
-populationSize = 10
+numSensorNeurons = 4
+numMotorNeurons = 4
+
+motorJointRange = 0.7
+
 
 amplitude, frequency, phaseOffset = numpy.pi/4,5, 0
 amplitude_fl, frequency_fl, phaseOffset_fl = numpy.pi/4, 10, 0
--- robot.py
+++ robot.py
@@ -1,5 +1,6 @@
 import pyrosim.pyrosim as pyrosim
 import pybullet as p
+import constants as c
 import os
 
 from pyrosim.neuralNetwork import NEURAL_NETWORK
@@ -36,7 +37,7 @@ class ROBOT:
         for neuronName in self.nn.Get_Neuron_Names():
             if self.nn.Is_Motor_Neuron(neuronName):
                 jointName = self.nn.Get_Motor_Neurons_Joint(neuronName)
-                desiredAngle = self.nn.Get_Value_Of(neuronName)
+                desiredAngle = self.nn.Get_Value_Of(neuronName) * c.motorJointRange
                 self.motors[jointName.encode("utf-8")].Set_Value(desiredAngle, self.robotId)
                 
     def Think(self):
